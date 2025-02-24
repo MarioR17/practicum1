b2cab3fe49a0755eedf1559e92c683fd45825987
MarioR17 <jmrendonq17@gmail.com>
2025-02-23 22:44:03 -0500

    half of practicum2

---
+++ src/main/java/edu/iu/p466/prime_service/security/SecurityConfig.java
@@ -0,0 +1,34 @@
+/*
+package edu.iu.p466.prime_service.security;
+
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.context.SecurityContext;
+
+import java.security.interfaces.RSAKey;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+    private RSAKey rsaKey;
+
+    public SecurityConfig() {
+        this.rsaKey = Jwks.generateRsa();
+    }
+
+
+    @Bean
+    public JWKSource<SecurityContext> jwkSource() {
+        JWKSet jwkSet = new JWKSet(rsaKey);
+        return (jwkSelector, securityContext) -> jwkSelector.select(jwkSet);
+    }
+}
+
+
+
+ */
