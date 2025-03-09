d7df1e0fc07fefc637362cf15ddea68a5946454a
MarioR17 <jmrendonq17@gmail.com>
2025-03-08 20:27:56 -0500

    finished prime-service stuff up to practicum4 but with errors

--- src/main/java/edu/iu/p466/prime_service/model/Customer.java
+++ src/main/java/edu/iu/p466/prime_service/model/Customer.java
@@ -1,8 +1,18 @@
 package edu.iu.p466.prime_service.model;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Table
+@Entity
 public class Customer {
+    @Id
     private String username;
     private String password;
+
+    public Customer() {}
 
     public Customer(String username, String password) {
         this.username = username;
